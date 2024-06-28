e27a7578134cc5f317da07cefcc02904712b29fc
Paz <165732644+06urania@users.noreply.github.com>
2024-06-28 08:58:21 -0400

    Create carrusel.js

---
+++ ProyectoProgramacion/domos/static/js/carrusel.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var carousel = new bootstrap.Carousel(
+    document.querySelector("#carouselExampleControls"),
+    {
+      interval: 2000,
+    }
+  )
+})
